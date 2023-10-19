27be4684aa72b5c89bfb3051222f805ceb35ee2d
Lucas Nobre Barbosa <lucasbnobre18@gmail.com>
2023-10-18 22:19:40 -0300

    feat: add first step fase in fl (client side fit and evaluate)

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from server import GlobalServer
+from client import HouseClient
 import utils
 
 server = GlobalServer()
+client1 = HouseClient()
+client2 = HouseClient()
+
+print(f'{server.model.estimators_}\n{client1.local_model.estimators_}\n{client2.local_model.estimators_}')
+
+(error1, accuracy1, best_trees1) = client1.evaluate(server.model)
+(error2, accuracy2, best_trees2) = client2.evaluate(server.model)
+
+print(error1, accuracy1)
+print(error2, accuracy2)
--- client.py
+++ client.py
@@ -17,28 +17,31 @@ class HouseClient():
         (self.X_train, self.y_train) = utils.partition(X_train, y_train, 2)[partition_id]
 
         # Initialize local model and set initial_parameters
-        self.model = RandomForestRegressor(n_estimators=TREES_BY_CLIENT)
-        utils.set_initial_params(self.model, X_train, y_train)
-        self.trees = self.model.estimators_
+        self.local_model = RandomForestRegressor(n_estimators=TREES_BY_CLIENT)
+        utils.set_initial_params(self.local_model, X_train, y_train) 
+        self.trees = self.local_model.estimators_
 
     def get_global_parameters(self, global_model):
             return utils.get_model_parameters(global_model)
 
     def evaluate(self, global_model):
         global_model_trees = self.get_global_parameters(global_model)
-        local_error = mean_absolute_error(self.y_test, self.model.predict(self.X_test))
-        utils.set_model_params(self.model, )
-        global_model_error = mean_absolute_error(self.y_test, self.model.predict(self.X_test))
+        
+        local_error = mean_absolute_error(self.y_test, self.local_model.predict(self.X_test))
+        global_model_error = mean_absolute_error(self.y_test, global_model.predict(self.X_test))
+
         if local_error < global_model_error:
              print('Erro local é menor')
-             utils.set_initial_params(self.model, self.trees)
              error = local_error
         else:
              print('Erro global é menor')
              error = global_model_error
              self.trees = global_model_trees
-        accuracy = self.model.score(self.X_test, self.y_test)
-        return error, len(self.X_test), {"accuracy": accuracy}, self.trees
+             utils.set_model_params(self.local_model, self.trees)
+            
+        accuracy = self.local_model.score(self.X_test, self.y_test)
+
+        return error, accuracy, self.trees
 
     
         
